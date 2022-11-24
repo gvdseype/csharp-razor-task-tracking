8dfd51e443eb4d86cd92351a91213cf5c6b2ce84
Gilles Vanderseypen <gilles.vanderseypen@extern.atrias.be>
2022-11-24 13:35:04 +0100

    Completed module

--- Tasker/Pages/Index.cshtml.cs
+++ Tasker/Pages/Index.cshtml.cs
@@ -1,3 +1,6 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,14 +9,17 @@ namespace Tasker.Pages
     public class IndexModel : PageModel
     {
         public IEnumerable<Task> Tasks;
-        public void OnGet()
-        {
-        }
+        
 
         private ApplicationDbContext _context;
         public IndexModel(ApplicationDbContext context)
         {
             context = _context;
+        }
+
+        public void OnGet()
+        {
+            Tasks = _context.Tasks.OrderBy(x => x.Priority);
         }
     }
 }

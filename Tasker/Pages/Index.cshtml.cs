f253b9d14629c9eba0cde8c579244362f4fcafd7
Gilles Vanderseypen <gilles.vanderseypen@extern.atrias.be>
2022-11-24 13:31:50 +0100

    Completed module

--- Tasker/Pages/Index.cshtml.cs
+++ Tasker/Pages/Index.cshtml.cs
@@ -5,8 +5,15 @@ namespace Tasker.Pages
 {
     public class IndexModel : PageModel
     {
+        public IEnumerable<Task> Tasks;
         public void OnGet()
         {
+        }
+
+        private ApplicationDbContext _context;
+        public IndexModel(ApplicationDbContext context)
+        {
+            context = _context;
         }
     }
 }
